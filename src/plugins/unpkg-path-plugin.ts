790949eebe07ad090273c9fd7b7703022bd3cdd7
ismail harmanda <iharmanda@hotmail.com>
2021-11-27 16:02:54 +0300

    detch modules dynamically

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,11 +1,20 @@
 import * as esbuild from "esbuild-wasm";
+import axios from "axios";
 
 export const unpkgPathPlugin = () => {
   return {
     name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log("onResole", args);
+        console.log("onResolve", args);
+        if (args.path === "index.js") {
+          return { path: args.path, namespace: "a" };
+        } else if (args.path === "tiny-test-pkg") {
+          return {
+            path: "https://unpkg.com/tiny-test-pkg@1.0.0/index.js",
+            namespace: "a",
+          };
+        }
         return { path: args.path, namespace: "a" };
       });
 
@@ -16,11 +25,17 @@ export const unpkgPathPlugin = () => {
           return {
             loader: "jsx",
             contents: `
-              import message from 'tiny-test-pkg';
+              const message = require('tiny-test-pkg');
               console.log(message);
             `,
           };
         }
+
+        const { data } = await axios.get(args.path);
+        return {
+          loader: "jsx",
+          contents: data,
+        };
       });
     },
   };
