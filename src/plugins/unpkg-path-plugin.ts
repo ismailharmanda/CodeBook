ba206aa0d7837f42caeef856507d203a0a29d94b
ismail harmanda <iharmanda@hotmail.com>
2021-11-27 16:21:52 +0300

    generate the Unpkg URL

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -9,13 +9,18 @@ export const unpkgPathPlugin = () => {
         console.log("onResolve", args);
         if (args.path === "index.js") {
           return { path: args.path, namespace: "a" };
-        } else if (args.path === "tiny-test-pkg") {
-          return {
-            path: "https://unpkg.com/tiny-test-pkg@1.0.0/index.js",
-            namespace: "a",
-          };
         }
-        return { path: args.path, namespace: "a" };
+
+        return {
+          namespace: "a",
+          path: `https://unpkg.com/${args.path}`,
+        };
+        // else if (args.path === "tiny-test-pkg") {
+        //   return {
+        //     path: "https://unpkg.com/tiny-test-pkg@1.0.0/index.js",
+        //     namespace: "a",
+        //   };
+        // }
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
