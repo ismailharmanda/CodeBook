6e8d0f2c0d73f432a67f4e2cf32d38333723135f
ismail harmanda <iharmanda@hotmail.com>
2021-11-27 18:05:19 +0300

    Resolve Nested Paths

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -14,18 +14,15 @@ export const unpkgPathPlugin = () => {
         if (args.path.includes("./") || args.path.includes("../"))
           return {
             namespace: "a",
-            path: new URL(args.path, args.importer + "/").href,
+            path: new URL(
+              args.path,
+              "https://unpkg.com" + args.resolveDir + "/"
+            ).href,
           };
         return {
           namespace: "a",
           path: `https://unpkg.com/${args.path}`,
         };
-        // else if (args.path === "tiny-test-pkg") {
-        //   return {
-        //     path: "https://unpkg.com/tiny-test-pkg@1.0.0/index.js",
-        //     namespace: "a",
-        //   };
-        // }
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -41,10 +38,13 @@ export const unpkgPathPlugin = () => {
           };
         }
 
-        const { data } = await axios.get(args.path);
+        const { data, request } = await axios.get(args.path);
+        console.log(request);
+
         return {
           loader: "jsx",
           contents: data,
+          resolveDir: new URL("./", request.responseURL).pathname,
         };
       });
     },
