28be6639533fd384ad8ff809738c96e187dd9e7c
ismail harmanda <iharmanda@hotmail.com>
2021-11-27 17:34:34 +0300

    use URL constructor

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -11,6 +11,11 @@ export const unpkgPathPlugin = () => {
           return { path: args.path, namespace: "a" };
         }
 
+        if (args.path.includes("./") || args.path.includes("../"))
+          return {
+            namespace: "a",
+            path: new URL(args.path, args.importer + "/").href,
+          };
         return {
           namespace: "a",
           path: `https://unpkg.com/${args.path}`,
@@ -30,7 +35,7 @@ export const unpkgPathPlugin = () => {
           return {
             loader: "jsx",
             contents: `
-              const message = require('tiny-test-pkg');
+              const message = require('medium-test-pkg');
               console.log(message);
             `,
           };
