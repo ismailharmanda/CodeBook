57f62893e4b20062d9bf45199036a4157de43da3
ismail harmanda <iharmanda@hotmail.com>
2021-11-28 18:33:03 +0300

    escaping CSS snippets

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -28,11 +28,16 @@ export const fetchPlugin = (inputCode: string) => {
         const { data, request } = await axios.get(args.path);
 
         const fileType = args.path.match(/.css$/) ? "css" : "jsx";
+
+        const escaped = data
+          .replace(/\n/g, "")
+          .replace(/"/g, '\\"')
+          .replace(/'/g, "\\'");
         const contents =
           fileType === "css"
             ? `
             const style = document.createElement('style');
-            style.innerText = 'body { background-color: "red" }';
+            style.innerText = '${escaped}';
             document.head.appendChild(style);
           `
             : data;
