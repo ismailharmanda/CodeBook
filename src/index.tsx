b964af1fbdd04bda06917c3d91f2e7320cf17c67
ismail harmanda <iharmanda@hotmail.com>
2021-11-28 15:41:58 +0300

    Refactore Multiple Plugins

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import * as esbuild from "esbuild-wasm";
 import { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
 import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
+import { fetchPlugin } from "./plugins/fetch-plugin";
 
 const App = () => {
   const ref = useRef<any>();
@@ -14,7 +15,6 @@ const App = () => {
       wasmURL: "/esbuild.wasm",
     });
   };
-
   useEffect(() => {
     startService();
   }, []);
@@ -23,21 +23,23 @@ const App = () => {
     if (!ref.current) {
       return;
     }
+
     const result = await ref.current.build({
       entryPoints: ["index.js"],
       bundle: true,
       write: false,
-      plugins: [unpkgPathPlugin(input)],
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
       define: {
         "process.env.NODE_ENV": '"production"',
         global: "window",
       },
     });
 
-    console.log(result);
+    // console.log(result);
 
     setCode(result.outputFiles[0].text);
   };
+
   return (
     <div>
       <textarea
