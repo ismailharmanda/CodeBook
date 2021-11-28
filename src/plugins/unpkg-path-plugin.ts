631982fc100c41d869db47c94bbf98241ecc173a
ismail harmanda <iharmanda@hotmail.com>
2021-11-28 15:20:09 +0300

    break up resolve logic with filters

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -10,20 +10,22 @@ export const unpkgPathPlugin = (inputCode: string) => {
   return {
     name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
-      build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log("onResolve", args);
-        if (args.path === "index.js") {
-          return { path: args.path, namespace: "a" };
-        }
+      // Handle root entry file of 'index.js'
+      build.onResolve({ filter: /(^index\.js$)/ }, () => {
+        return { path: "index.js", namespace: "a" };
+      });
 
-        if (args.path.includes("./") || args.path.includes("../"))
-          return {
-            namespace: "a",
-            path: new URL(
-              args.path,
-              "https://unpkg.com" + args.resolveDir + "/"
-            ).href,
-          };
+      // Handle relative paths in a module
+      build.onResolve({ filter: /^\.+\// }, (args: any) => {
+        return {
+          namespace: "a",
+          path: new URL(args.path, "https://unpkg.com" + args.resolveDir + "/")
+            .href,
+        };
+      });
+
+      // Handle main file of a module
+      build.onResolve({ filter: /.*/ }, async (args: any) => {
         return {
           namespace: "a",
           path: `https://unpkg.com/${args.path}`,
@@ -40,27 +42,20 @@ export const unpkgPathPlugin = (inputCode: string) => {
           };
         }
 
-        // Check to see if we have already fetched this file
-        // and if it is in the cache
         const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
           args.path
         );
 
-        //if it is, return it immediately
         if (cachedResult) {
           return cachedResult;
         }
-
         const { data, request } = await axios.get(args.path);
-        // store response in cache
 
         const result: esbuild.OnLoadResult = {
           loader: "jsx",
           contents: data,
           resolveDir: new URL("./", request.responseURL).pathname,
         };
-
-        // store response in cache
         await fileCache.setItem(args.path, result);
 
         return result;
