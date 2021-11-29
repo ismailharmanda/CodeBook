185c2a161f8df65150ed7ce56cb505e43b09dc60
ismail harmanda <iharmanda@hotmail.com>
2021-11-29 14:09:01 +0300

    implement "sandbox" in iFrame(no longer directly reachable)

--- src/index.tsx
+++ src/index.tsx
@@ -50,7 +50,7 @@ const App = () => {
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
-      <iframe src="/test.html" />
+      <iframe sandbox="" src="/test.html" />
     </div>
   );
 };
