3f68f526f4bcf12c54cd832df62efadbede728de
ismail harmanda <iharmanda@hotmail.com>
2021-12-04 12:27:18 +0300

    configure a few more options

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -8,6 +8,13 @@ const CodeEditor = () => {
       height="500px"
       options={{
         wordWrap: "on",
+        minimap: { enabled: false },
+        showUnused: true,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: false,
+        automaticLayout: true,
       }}
     />
   );
