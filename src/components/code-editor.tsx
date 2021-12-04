febbc7dd650ed72e1c8f38aa3db2522535d67eb4
ismail harmanda <iharmanda@hotmail.com>
2021-12-04 12:20:33 +0300

    configure language, theme and wordWrap options

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,7 +1,16 @@
 import MonacoEditor from "@monaco-editor/react";
 
 const CodeEditor = () => {
-  return <MonacoEditor height="500px" />;
+  return (
+    <MonacoEditor
+      language="javascript"
+      theme="dark"
+      height="500px"
+      options={{
+        wordWrap: "on",
+      }}
+    />
+  );
 };
 
 export default CodeEditor;
