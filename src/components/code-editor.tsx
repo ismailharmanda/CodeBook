4c8045ba56902b1bd64faf80fe4d11d5d4638b78
ismail harmanda <iharmanda@hotmail.com>
2021-12-04 16:01:56 +0300

    add bulma css library and try it in button

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -39,7 +39,12 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
 
   return (
     <div>
-      <button onClick={onFormatClick}>Format</button>
+      <button
+        className="button button-format is-primary is-small"
+        onClick={onFormatClick}
+      >
+        Format
+      </button>
       <MonacoEditor
         editorDidMount={onEditorDidMount}
         value={initialValue}
